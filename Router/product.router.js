05bf086492559821434835a6e62a8849c0b7b019
utkarshqw <utkarshup75@gmail.com>
2022-12-15 13:18:27 +0530

    update2

--- Router/product.router.js
+++ Router/product.router.js
@@ -1,6 +1,28 @@
-const express = require("express")
+const express = require("express");
+const bookmarkModel = require("../Schema/Bookmark.model");
 const productModel = require("../Schema/Product.model") 
 const app = express.Router();
+
+app.get("/",async(req,res)=>{
+    var x = await productModel.find()
+    res.send(x)
+})
+
+
+app.delete("/delete",async(req,res)=>{
+    var {_id} = req.body
+    var x  = await productModel.findByIdAndDelete(_id)
+    res.send("deleted")
+})
+
+app.post("/bookmark",async(req,res)=>{
+
+    let newproduct = new bookmarkModel(req.body)
+    await newproduct.save()
+    res.send("success")
+
+})
+
 
 app.post("/add",async(req,res)=>{
     
